9ca6402cd9b1cb3de5c109b0d9e71e0f70c0b179
CR4SH3D <63345758+crashixx@users.noreply.github.com>
2022-08-27 19:02:11 +0200

    Death recorder implementation

---
+++ susint_src/utils/deathrecord/deathrecord.py
@@ -0,0 +1,23 @@
+from stat import FILE_ATTRIBUTE_NO_SCRUB_DATA
+from utils.plugin.susint_base import *
+
+def deathrecorder(firstname, name, profile_found):
+    response = get(f"https://www.libramemoria.com/avis?nom={name}&prenom={firstname}")    
+    content = response.content
+    parser = "html.parser"
+    site = BeautifulSoup(content, parser)
+
+    names  = site.find_all('div',{'class':'cellule nom alone'})
+    ages   = site.find_all('div',{'class':'cellule age hideXs_tablecell alone'})
+    villes = site.find_all('div',{'class':'cellule ville liste_virgule alone'})
+
+    for x in range(len(names)):
+        try:
+            _name = names[x].text.split('(')[0].replace('\r','').replace('\n','').replace('\t','').strip()
+            age = ages[x].text.replace("ans","Years old").strip()
+            ville = villes[x].text.replace('\r','').replace('\n','').replace('\t','').replace('(',' (').strip()
+            all = f"Name: {_name}\nAge: {age}\nVille: {ville}"
+            profile_found.append(all)
+        except:
+            pass
+        
