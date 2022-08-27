838fa697c3aaf116091a3d51f5e6594d5d1356f0
CR4SH3D <63345758+crashixx@users.noreply.github.com>
2022-08-27 19:04:19 +0200

    mailchecker optimizations

--- susint_src/utils/mailchecker/mail_checker.py
+++ susint_src/utils/mailchecker/mail_checker.py
@@ -1,32 +1,14 @@
 from utils.plugin.susint_base import *
 
-def mail_checker(usr1, usr2, usr3, usr4, usr5, usr6, usr7, usr8, api_keys, valid_mails):
+def mail_checker(users, api_keys, valid_mails):
     
     mails = []
+    host = ["@yandex.ru","@hotmail.com","@outlook.com","@yahoo.com","@gmail.com","@icloud.com","@aol.com","@msn.com","@live.com"]
 
-    #hostlist
-    host1 = "@gmail.com"
-    host2 = "@orange.fr"
+    for user in users:
+        for h in range(len(host)):
+            mails.append(f"{user}{host[h]}")
 
-    # mail list host 1
-    mails.append(f"{usr1}{host1}")
-    mails.append(f"{usr2}{host1}")
-    mails.append(f"{usr3}{host1}")
-    mails.append(f"{usr4}{host1}")
-    mails.append(f"{usr5}{host1}")
-    mails.append(f"{usr6}{host1}")
-    mails.append(f"{usr7}{host1}")
-    mails.append(f"{usr8}{host1}")
-
-    #mail list host 2
-    mails.append(f"{usr1}{host2}")
-    mails.append(f"{usr2}{host2}")
-    mails.append(f"{usr3}{host2}")
-    mails.append(f"{usr4}{host2}")
-    mails.append(f"{usr5}{host2}")
-    mails.append(f"{usr6}{host2}")
-    mails.append(f"{usr7}{host2}")
-    mails.append(f"{usr8}{host2}")
 
     #email checker
     for email in mails:
@@ -35,5 +17,5 @@ def mail_checker(usr1, usr2, usr3, usr4, usr5, usr6, usr7, usr8, api_keys, valid
         status = response.json()['status']
         if status == "valid":
             valid_mails.append(email)
-        elif status == "invalid":
+        else:
             pass
