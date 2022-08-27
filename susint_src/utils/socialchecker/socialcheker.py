98e6c2b108877975d9c96aa7745fe1f495c21c0c
CR4SH3D <63345758+crashixx@users.noreply.github.com>
2022-08-27 19:05:55 +0200

    social checker fixs & optimizations

--- susint_src/utils/socialchecker/socialcheker.py
+++ susint_src/utils/socialchecker/socialcheker.py
@@ -1,53 +1,19 @@
 from utils.plugin.susint_base import *
 
-def socialchecker(socials, working_socials, usr1, usr2, usr3, usr4, usr5, usr6, usr7, usr8):
+def socialchecker(socials, users, working_socials ):
     
+    #instgram dedicated checker
+    for user in users:
+        res = get(url = f"https://www.instagram.com/{user}")
+        soup = BeautifulSoup(res.text, "html.parser")
+        if soup(text=lambda t: user in t.text) != []:
+            working_socials.append(f'https://www.instagram.com/{user}')
+
+
     for social in socials:
-
-        res1 = get(social + usr1)
-        if res1.status_code == int(200):
-            working_socials.append(social + usr1)
-        else:
-            pass
-
-        res2 = get(social + usr2)
-        if res2.status_code == int(200):
-            working_socials.append(social + usr2)
-        else:
-            pass
-
-        res3 = get(social + usr3)
-        if res3.status_code == int(200):
-            working_socials.append(social + usr3)
-        else:
-            pass
-
-        res4 = get(social + usr4)
-        if res4.status_code == int(200):
-            working_socials.append(social + usr4)
-        else:
-            pass
-
-        res5 = get(social + usr5)
-        if res5.status_code == int(200):
-            working_socials.append(social + usr5)
-        else:
-            pass
-
-        res6 = get(social + usr6)
-        if res6.status_code == int(200):
-            working_socials.append(social + usr6)
-        else:
-            pass
-
-        res7 = get(social + usr7)
-        if res7.status_code == int(200):
-            working_socials.append(social + usr7)
-        else:
-            pass
-
-        res8 = get(social + usr8)
-        if res8.status_code == int(200):
-            working_socials.append(social + usr8)
-        else:
-            pass
+        for user in users:
+            res = get(social + user)
+            if res.status_code == 200:
+                working_socials.append(social + user)
+            elif res.status_code == 404:
+                pass
